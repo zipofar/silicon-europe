771c36fd92285a6638b646adfdd72daeed53a31e
Zipofar <zipofar@gmail.com>
2019-03-03 23:37:58 +0300

    Fix router

--- src/reducers/routes.js
+++ src/reducers/routes.js
@@ -1,3 +1,3 @@
-const host = 'https://morning-atoll-29670.herokuapp.com/';
+const host = process.env.REACT_APP_HOST || 'http://localhost:9000;
 export const routeLegalEntity = () => (host + '/legal_entities');
 export const routePharmacy = id => (host + '/legal_entities/' + id + '/pharmacies');
